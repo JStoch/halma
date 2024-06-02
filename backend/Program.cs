75a879c7ae3d7223bbd76e85c4eba74c77c4cce8
Krzysztof Wróblewski <260394@student.pwr.edu.pl>
2024-06-02 20:53:50 +0200

    Resolved conflicts, ready to merge

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,72 +8,47 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 internal class Program
 {
-
-
-    
-        
     private static void Main(string[] args)
     {
-var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+        // Add services to the container.
 
         builder.Services.AddControllers();
 
-       
+
 
         var halmaDbConnString = builder.Configuration.GetConnectionString("HalmaDbConn");
-        builder.Services.AddDbContext<HalmaDbContext>(opt => { opt.UseSqlServer(halmaDbConnString); opt.EnableDetailedErrors(); }) ;
+        builder.Services.AddDbContext<HalmaDbContext>(opt => { opt.UseSqlServer(halmaDbConnString); opt.EnableDetailedErrors(); });
 
         //var identityServerDbConnString = builder.Configuration.GetConnectionString("IdentityServerDbConn");
-        
 
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSignalR();
-builder.Services.AddSingleton<GameRepository>();
-builder.Services.AddSingleton<GameService>();
 
-var app = builder.Build();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddSignalR();
+        builder.Services.AddSingleton<GameRepository>();
+        builder.Services.AddSingleton<GameService>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        var app = builder.Build();
 
-app.UseHttpsRedirection();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+        app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
-app.MapHub<GameHub>("/game");
+
+        app.MapHub<GameHub>("/game");
         app.MapControllers();
 
-app.Run();
+        app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
